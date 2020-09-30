4ef242c14a09d601fc46b10b3243cee1c6a0fcb3
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-30 15:03:42 +0530

    Initial Commit

---
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -0,0 +1,9 @@
+package com.userregistration;
+
+public class UserRegistration 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "User Registration Test Cases" );
+    }
+}
---
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -0,0 +1,14 @@
+package com.userregistration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserRegistrationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
